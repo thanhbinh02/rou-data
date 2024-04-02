bb18e87ad5b13c1bc13383a13d82a7c50c5413c0
Thanh Binh <thanhbinh.dinh@estuary.solutions>
2024-04-02 15:22:47 +0700

    feat: setup layout

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExampleHauGiangMap from "./components/mekong/example-hau-giang-map";
+
+function App() {
+  return (
+    <>
+      <ExampleHauGiangMap />
+    </>
+  );
+}
+
+export default App;
