8433a4121ca2184edd94ac7f4b2cfe7d04269c69
Thanh Binh <thanhbinh.dinh@estuary.solutions>
2024-04-02 15:21:06 +0700

    feat: add json data

---
+++ src/data/color.ts
@@ -0,0 +1,72 @@
+import {
+  red,
+  volcano,
+  orange,
+  gold,
+  yellow,
+  lime,
+  green,
+  cyan,
+  blue,
+  purple,
+  magenta,
+} from "@ant-design/colors";
+
+export const COLOR_LIST = [
+  red[2],
+  volcano[2],
+  orange[2],
+  gold[2],
+  yellow[2],
+  lime[2],
+  green[2],
+  cyan[2],
+  blue[2],
+  purple[2],
+  magenta[2],
+  red[5],
+  volcano[5],
+  orange[5],
+  gold[5],
+  yellow[5],
+  lime[5],
+  green[5],
+  cyan[5],
+  blue[5],
+  purple[5],
+  magenta[5],
+  red[8],
+  volcano[8],
+  orange[8],
+  gold[8],
+  yellow[8],
+  lime[8],
+  green[8],
+  cyan[8],
+  blue[8],
+  purple[8],
+  magenta[8],
+];
+
+// series: {
+//   dataLabels: {
+//     enabled: true,
+//     formatter: function () {
+//       return (
+//         '<text x="0" y="0" style="font-size: 16px; font-weight: bold; text-transform: uppercase; color: "black"">' +
+//         '<tspan x="0" dy="1.2em">' +
+//         this.point.name +
+//         "</tspan>" +
+//         '<tspan x="0" dy="1.2em">' +
+//         this.point.series.userOptions.volumes +
+//         "</tspan>" +
+//         "</text>"
+//       );
+//     },
+//     style: {
+//       textOutline: "none",
+//     },
+//     verticalAlign: "middle",
+//     align: "center",
+//   },
+// },
