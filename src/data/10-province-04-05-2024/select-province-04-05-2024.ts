edfda6e4460c04bc50cbefa506958117d7bf65eb
Thanh Binh <thanhbinh.dinh@estuary.solutions>
2024-05-04 15:45:10 +0700

    feat: update data of Bắc Cạn, Bắc Giang, Bắc Ninh, Bình Định, Cao Bằng, Cần Thơ, Đà Nẵng, Đắk Lắk, Gia Lai, Hà Giang

---
+++ src/data/10-province-04-05-2024/select-province-04-05-2024.ts
@@ -0,0 +1,473 @@
+export const DISTRICTS_BAC_KAN = [
+  {
+    name: "Thành Phố Bắc Kạn",
+    code: "tp-bac-kan",
+  },
+  {
+    name: "Huyện Pác Nặm",
+    code: "pac-nam",
+  },
+  {
+    name: "Huyện Ba Bể",
+    code: "ba-be",
+  },
+  {
+    name: "Huyện Ngân Sơn",
+    code: "ngan-son",
+  },
+  {
+    name: "Huyện Bạch Thông",
+    code: "bach-thong",
+  },
+  {
+    name: "Huyện Chợ Đồn",
+    code: "cho-don",
+  },
+  {
+    name: "Huyện Chợ Mới",
+    code: "cho-moi",
+  },
+  {
+    name: "Huyện Na Rì",
+    code: "na-ri",
+  },
+];
+
+export const DISTRICTS_BAC_GIANG = [
+  {
+    name: "Thành phố Bắc Giang",
+    code: "tp-bac-giang",
+  },
+  {
+    name: "Huyện Yên Thế",
+    code: "yen-the",
+  },
+  {
+    name: "Huyện Tân Yên",
+    code: "tan-yen",
+  },
+  {
+    name: "Huyện Lạng Giang",
+    code: "lang-giang",
+  },
+  {
+    name: "Huyện Lục Nam",
+    code: "luc-nam",
+  },
+  {
+    name: "Huyện Lục Ngạn",
+    code: "luc-ngan",
+  },
+  {
+    name: "Huyện Sơn Động",
+    code: "son-dong",
+  },
+  {
+    name: "Huyện Yên Dũng",
+    code: "yen-dung",
+  },
+  {
+    name: "Huyện Hiệp Hòa",
+    code: "hiep-hoa",
+  },
+  {
+    name: "Huyện Việt Yên",
+    code: "viet-yen",
+  },
+];
+
+export const DISTRICTS_BAC_NINH = [
+  {
+    name: "Thành phố Bắc Ninh",
+    code: "tp-bac-ninh",
+  },
+  {
+    name: "Huyện Yên Phong",
+    code: "yen-phong",
+  },
+  {
+    name: "Huyện Quế Võ",
+    code: "que-vo",
+  },
+  {
+    name: "Huyện Tiên Du",
+    code: "tien-du",
+  },
+  {
+    name: "Thành phố Từ Sơn",
+    code: "tp-tu-son",
+  },
+  {
+    name: "Huyện Thuận Thành",
+    code: "thuan-thanh",
+  },
+  {
+    name: "Huyện Gia Bình",
+    code: "gia-binh",
+  },
+  {
+    name: "Huyện Lương Tài",
+    code: "luong-tai",
+  },
+];
+
+export const DISTRICTS_BINH_DINH = [
+  {
+    name: "Thành phố Quy Nhơn",
+    code: "tp-quy-nhon",
+  },
+  {
+    name: "Huyện An Lão",
+    code: "an-lao",
+  },
+  {
+    name: "Thị xã Hoài Nhơn",
+    code: "hoai-nhon",
+  },
+  {
+    name: "Huyện Hoài Ân",
+    code: "hoai-an",
+  },
+  {
+    name: "Huyện Phù Mỹ",
+    code: "phu-my",
+  },
+  {
+    name: "Huyện Vĩnh Thạnh",
+    code: "vinh-thanh",
+  },
+  {
+    name: "Huyện Tây Sơn",
+    code: "tay-son",
+  },
+  {
+    name: "Huyện Phù Cát",
+    code: "phu-cat",
+  },
+  {
+    name: "Thị xã An Nhơn",
+    code: "an-nhon",
+  },
+  {
+    name: "Huyện Tuy Phước",
+    code: "tuy-phuoc",
+  },
+  {
+    name: "Huyện Vân Canh",
+    code: "van-canh",
+  },
+];
+
+export const DISTRICTS_CAO_BANG = [
+  {
+    name: "Thành phố Cao Bằng",
+    code: "tp-cao-bang",
+  },
+  {
+    name: "Huyện Bảo Lâm",
+    code: "bao-lam",
+  },
+  {
+    name: "Huyện Bảo Lạc",
+    code: "bao-lac",
+  },
+  {
+    name: "Huyện Hà Quảng",
+    code: "ha-quang",
+  },
+  {
+    name: "Huyện Trùng Khánh",
+    code: "trung-khanh",
+  },
+  {
+    name: "Huyện Hạ Lang",
+    code: "ha-lang",
+  },
+  {
+    name: "Huyện Cờ Đỏ",
+    code: "co-do",
+  },
+  {
+    name: "Huyện Hoà An",
+    code: "hoa-an",
+  },
+  {
+    name: "Huyện Nguyên Bình",
+    code: "nguyen-binh",
+  },
+  {
+    name: "Huyện Thạch An",
+    code: "thach-an",
+  },
+  {
+    name: "Huyện Thông Nông",
+    code: "thong-nong",
+  },
+  {
+    name: "Huyện Phục Hoà",
+    code: "phuc-hoa",
+  },
+  {
+    name: "Huyện Trà Lĩnh",
+    code: "tra-linh",
+  },
+];
+
+export const DISTRICTS_CAN_THO = [
+  {
+    name: "Quận Ninh Kiều",
+    code: "ninh-kieu",
+  },
+  {
+    name: "Quận Ô Môn",
+    code: "o-mon",
+  },
+  {
+    name: "Quận Bình Thuỷ",
+    code: "binh-thuy",
+  },
+  {
+    name: "Quận Cái Răng",
+    code: "cai-rang",
+  },
+  {
+    name: "Quận Thốt Nốt",
+    code: "thot-not",
+  },
+  {
+    name: "Huyện Vĩnh Thạnh",
+    code: "vinh-thanh",
+  },
+  {
+    name: "Huyện Cờ Đỏ",
+    code: "co-do",
+  },
+  {
+    name: "Huyện Phong Điền",
+    code: "phong-dien",
+  },
+  {
+    name: "Huyện Thới Lai",
+    code: "thoi-lai",
+  },
+];
+
+export const DISTRICTS_DA_NANG = [
+  {
+    name: "Quận Liên Chiểu",
+    code: "lien-chieu",
+  },
+  {
+    name: "Quận Thanh Khê",
+    code: "thanh-khe",
+  },
+  {
+    name: "Quận Hải Châu",
+    code: "hai-chau",
+  },
+  {
+    name: "Quận Sơn Trà",
+    code: "son-tra",
+  },
+  {
+    name: "Quận Ngũ Hành Sơn",
+    code: "ngu-hanh-son",
+  },
+  {
+    name: "Quận Cẩm Lệ",
+    code: "cam-le",
+  },
+  {
+    name: "Huyện Hòa Vang",
+    code: "hoa-vang",
+  },
+  {
+    name: "Huyện Hoàng Sa",
+    code: "hoang-sa",
+  },
+  {
+    name: "Huyện Thới Lai",
+    code: "thoi-lai",
+  },
+];
+
+export const DISTRICTS_DAK_LAK = [
+  {
+    name: "Thành phố Buôn Ma Thuột",
+    code: "buon-ma-thuot",
+  },
+  {
+    name: "Thị Xã Buôn Hồ",
+    code: "buon-ho",
+  },
+  {
+    name: "Huyện Ea H'leo",
+    code: "ea-h-leo",
+  },
+  {
+    name: "Huyện Ea Súp",
+    code: "ea-sup",
+  },
+  {
+    name: "Huyện Buôn Đôn",
+    code: "buon-don",
+  },
+  {
+    name: "Huyện Cư M'gar",
+    code: "cu-m-gar",
+  },
+  {
+    name: "Huyện Krông Búk",
+    code: "krong-buk",
+  },
+  {
+    name: "Huyện Krông Năng",
+    code: "krong-nang",
+  },
+  {
+    name: "Huyện Ea Kar",
+    code: "ea-kar",
+  },
+  {
+    name: "Huyện M'Đrắk",
+    code: "m-drak",
+  },
+  {
+    name: "Huyện Krông Bông",
+    code: "krong-bong",
+  },
+  {
+    name: "Huyện Krông Pắc",
+    code: "krong-pac",
+  },
+  {
+    name: "Huyện Krông A Na",
+    code: "krong-a-na",
+  },
+  {
+    name: "Huyện Lắk",
+    code: "lak",
+  },
+  {
+    name: "Huyện Cư Kuin",
+    code: "cu-kuin",
+  },
+];
+
+export const DISTRICTS_GIA_LAI = [
+  {
+    name: "Thành phố Pleiku",
+    code: "pleiku",
+  },
+  {
+    name: "Thị xã An Khê",
+    code: "an-khe",
+  },
+  {
+    name: "Thị xã Ayun Pa",
+    code: "ayun-pa",
+  },
+  {
+    name: "Huyện KBang",
+    code: "kbang",
+  },
+  {
+    name: "Huyện Đăk Đoa",
+    code: "dak-doa",
+  },
+  {
+    name: "Huyện Chư Păh",
+    code: "chu-pah",
+  },
+  {
+    name: "Huyện Ia Grai",
+    code: "ia-grai",
+  },
+  {
+    name: "Huyện Mang Yang",
+    code: "mang-yang",
+  },
+  {
+    name: "Huyện Kông Chro",
+    code: "kong-chro",
+  },
+  {
+    name: "Huyện Đức Cơ",
+    code: "duc-co",
+  },
+  {
+    name: "Huyện Chư Prông",
+    code: "chu-prong",
+  },
+  {
+    name: "Huyện Chư Sê",
+    code: "chu-se",
+  },
+  {
+    name: "Huyện Đăk Pơ",
+    code: "dak-po",
+  },
+  {
+    name: "Huyện Ia Pa",
+    code: "ia-pa",
+  },
+  {
+    name: "Huyện Krông Pa",
+    code: "krong-pa",
+  },
+  {
+    name: "Huyện Phú Thiện",
+    code: "phu-thien",
+  },
+  {
+    name: "Huyện Chư Pưh",
+    code: "chu-puh",
+  },
+];
+
+export const DISTRICTS_HA_GIANG = [
+  {
+    name: "Thành phố Hà Giang",
+    code: "tp-ha-giang",
+  },
+  {
+    name: "Huyện Đồng Văn",
+    code: "dong-van",
+  },
+  {
+    name: "Huyện Mèo Vạc",
+    code: "meo-vac",
+  },
+  {
+    name: "Huyện Yên Minh",
+    code: "yen-minh",
+  },
+  {
+    name: "Huyện Quản Bạ",
+    code: "quan-ba",
+  },
+  {
+    name: "Huyện Vị Xuyên",
+    code: "vi-xuyen",
+  },
+  {
+    name: "Huyện Bắc Mê",
+    code: "bac-me",
+  },
+  {
+    name: "Huyện Hoàng Su Phì",
+    code: "hoang-su-phi",
+  },
+  {
+    name: "Huyện Xín Mần",
+    code: "xin-man",
+  },
+  {
+    name: "Huyện Bắc Quang",
+    code: "bac-quang",
+  },
+  {
+    name: "Huyện Quang Bình",
+    code: "quang-binh",
+  },
+];
